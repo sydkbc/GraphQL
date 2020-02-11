a012f8d307f0ee808db9222d14c6431a8d5f3210
asidiropoulos <a.sidiropoulos@blockachain.gr>
2020-02-11 02:11:41 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(5000, () => {
+  console.log("Server started on PORT:5000");
+});
+
+// Example Queries that can be tested under http://localhost:5000/graphQL //
+// Try one of the folllwoing queries                                      //
+//
+// {
+//     launches {
+//       flight_number,
+//       mission_name,
+//       launch_year,
+//       launch_date_local,
+//       launch_success,
+//       rocket {
+//         rocket_id
+//         rocket_name
+//         rocket_type
+//       }
+//     }
+//   }
+//
+// {
+//     launch(flight_number:3) {
+//       flight_number
+//       mission_name
+//       launch_year
+//       launch_date_local
+//       launch_success
+//     }
+//   }
+//
+// {
+//     rockets{
+//       rocket_id,
+//       rocket_name,
+//       rocket_type
+//     }
+//   }
+//
+// {
+//     rocket(id:2
+//     ) {
+//       rocket_id
+//       rocket_name
+//       rocket_type
+//     }
+//   }
